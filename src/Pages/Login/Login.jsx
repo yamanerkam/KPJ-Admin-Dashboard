cdec1d2207b00bd5c93f542a95af5c0f34b20469
Kam <erkamyaman35@gmail.com>
2024-05-08 20:29:29 +0300

    auth-step-1

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Amplify } from 'aws-amplify';
+import { getCurrentUser, signIn } from 'aws-amplify/auth';
+import awsExports from './../../aws-exports'
 import './Login.css'
+
+Amplify.configure(awsExports);
+
+const { username, userId, signInDetails } = await getCurrentUser();
+
+console.log("username", username);
+console.log("user id", userId);
+console.log("sign-in details", signInDetails);
+
 export default function Login() {
-    function handleSubmit(e) {
+    const [username, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const confirmationCode = '425564';
+    async function handleSubmit(e) {
         e.preventDefault()
+        try {
+            const { isSignedIn, nextStep } = await signIn({ username, password });
+            console.log('yes')
+        } catch (error) {
+            console.log('error signing in', error);
+        }
+        console.log(username, password)
     }
     return (
         <div className='login'>
             <div className="login-card">
                 <h1>Admin Login</h1>
                 <form onSubmit={handleSubmit} action="submit">
-                    <input required placeholder='Email' type="email" />
-                    <input required placeholder='Password' type="password" />
+                    <input required placeholder='Email' value={username} onChange={((e) => setUserName(e.target.value))} type="email" />
+                    <input placeholder='Password' value={password} onChange={((e) => setPassword(e.target.value))} type="password" />
                     <button type='submit'>Login</button>
                 </form>
 
