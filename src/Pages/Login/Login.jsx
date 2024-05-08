36e12e222719d664d29d8e53655119d37ea5b2f9
Kam <erkamyaman35@gmail.com>
2024-05-08 17:53:05 +0300

    login-page-final

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Login.css'
 export default function Login() {
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
     return (
         <div className='login'>
             <div className="login-card">
                 <h1>Admin Login</h1>
-                <input placeholder='Email' type="email" />
-                <input placeholder='Password' type="password" />
-                <button>Login</button>
+                <form onSubmit={handleSubmit} action="submit">
+                    <input required placeholder='Email' type="email" />
+                    <input required placeholder='Password' type="password" />
+                    <button type='submit'>Login</button>
+                </form>
+
 
             </div>
         </div>
